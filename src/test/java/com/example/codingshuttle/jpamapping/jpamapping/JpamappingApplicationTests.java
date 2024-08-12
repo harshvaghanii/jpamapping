f5bf3118141b1e19b2b0825be6c932da50bee2f8
harshvaghanii <harshvaghani98@gmail.com>
2024-08-12 13:44:23 -0400

    initial commit

---
+++ src/test/java/com/example/codingshuttle/jpamapping/jpamapping/JpamappingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codingshuttle.jpamapping.jpamapping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpamappingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
